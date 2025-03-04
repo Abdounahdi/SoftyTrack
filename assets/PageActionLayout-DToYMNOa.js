e8299b01c8e30f6c00abebba4632f693ce008e2d
Abdou <nahdi.abdo.2020@gmail.com>
2025-03-04 09:07:38 +0100

    Updates

---
+++ assets/PageActionLayout-DToYMNOa.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-G1bMk4i-.js";import{P as i}from"./PageHeading-CfkJVI-2.js";function s({children:o,goBack:t,title:e}){return a.jsxs("div",{className:"page_action_layout_container",children:[a.jsx(i,{title:e,goBack:t}),o]})}export{s as P};
---
+++ assets/Register-BDkrxSnJ.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-G1bMk4i-.js";const t=()=>e.jsx("div",{children:"Register"});export{t as default};
